6ae18876e1d7f0530c205983bb267270f23575c1
kannannagarajan126 <43824725+kannannagarajan126@users.noreply.github.com>
2018-12-18 22:19:04 +0530

    Add files via upload

--- server.R
+++ server.R
@@ -1,56 +1,124 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
+############################################################################
+#     Assginmnet 1:Shiny App around the UDPipe NLP workflow          #
+############################################################################
 
-library(shiny)
+library(lattice)
+library(igraph)
+library(ggraph)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  set.seed=13120018   
+shinyServer(function(input, output,session) {
+  set.seed=2092014   
+  options(shiny.maxRequestSize=30*1024^2)
   
   dataset <- reactive({
-    if (is.null(input$file)) {return(NULL)}
-    else {
-      text  = readLines(input$file$datapath)
-      text = text[text!=""]
-      
-      if (length(text[text !=""]) == 0 ) stop (print("Null Vector :( "))
-      
-      if (length(text) == 1) {
-        textdf = data_frame(text0 = text) %>% 
-          unnest_tokens(text, text0, token = "sentences")
-      } else {
-        textdf = data_frame(text = text)  
-      }
-      
+    
+    if (is.null(input$file)) {# locate 'file1' from ui.R
+      return(NULL) } 
+    else{
+      Data <- readLines(input$file$datapath)
+      Data  =  text.clean(Data)
+      return(Data)
     }
-    return(textdf)
+  })
+  
+  model_file = reactive({
+    file1<-input$udpipefile
+    if (is.null(file1)) {
+        return(NULL) 
+      } else{
+        model_file = udpipe_load_model(file1$datapath)
+        return(model_file)
+      }
+  })
+  
+  annotation = reactive({
+    x <- udpipe_annotate(model_file(),x = dataset())
+    x <- as.data.frame(x)
+    return(x)
+  })
+  
+  selectedPOS.obj = reactive({
+    return (input$POSselected)
+  })
+  
+  cooccurrence.obj <- reactive({
+    x<-annotation()
+    cooccurrence <- cooccurrence(x$lemma, relevant = x$upos %in% selectedPOS.obj(), skipgram = 1)
+    return (cooccurrence)
   })
   
   
-  dtm_tcm =  reactive({
+  output$selectedPOS  = renderPrint({
+    selectedPOS.obj()
+  })
+  
+  output$datatableOutput = renderDataTable({
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return(NULL)
+    }
+    else{
+      out = annotation()[,-4]
+      return(out)
+    }
+  })
+  
+  output$posFreq = renderPlot({
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return (NULL)
+    }else{
+      x<-annotation()
+      stats <- txt_freq(x$upos)
+      stats$key <- factor(stats$key, levels = rev(stats$key))
+      barchart(key ~ freq, data = stats, col = "blue", 
+               xlab = "Freq")
+    }
+      
+  })
+  
+  
+  
+  output$mostOcc  = renderPlot({
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return (NULL)
+    }else{
+      x<-annotation()
+      selected<-selectedPOS.obj()
+      stats <- subset(x, x$upos %in% selected) 
+      stats <- txt_freq(stats$token)
+      stats$key <- factor(stats$key, levels = rev(stats$key))
+      barchart(key ~ freq, data = head(stats, 20), col = "green", 
+               main = "Most occurring based on POS", xlab = "Freq")
+    }
     
-    textb = dataset()$Document
-    ids = dataset()$Doc.id
-    
-    dtm.tcm = dtm.tcm.creator(text = textb,
-                              id = ids,
-                              std.clean = TRUE,
-                              std.stop.words = TRUE,
-                              stop.words.additional = unlist(strsplit(input$stopw,",")),
-                              bigram.encoding = TRUE,
-                              # bigram.min.freq = 20,
-                              min.dtm.freq = input$freq,
-                              skip.grams.window = 10)
-    
-    # tcm = dtm.tcm$tcm
-    dtm_tcm_obj = list(dtm = dtm)#, tcm = tcm)
+  })
+  
+  
+  output$wordCloudFre  = renderPlot({
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return (NULL)
+    }else{
+      x<-annotation()
+      selected<-selectedPOS.obj()
+      subset_cl = x %>% subset(., upos %in% selected) 
+      subset_cl
+      sub_fre = txt_freq(subset_cl$lemma)
+      if(is.null(sub_fre))
+        {
+          return(NULL)
+        }
+        else{
+          wordcloud(sub_fre$key, sub_fre$freq,
+                  scale = c(4, 1),
+                  min.freq = 10,
+                  max.words = 50,
+                  colors = brewer.pal(8, "Dark2"))
+        }
+      }
   })
   
   wordcounts = reactive({
@@ -58,15 +126,44 @@ shinyServer(function(input, output) {
   }) 
   
   output$wordcloud <- renderPlot({
-    tsum = wordcounts()
-    tsum = tsum[order(tsum, decreasing = T)]
-    dtm.word.cloud(count = tsum,max.words = input$max,title = 'Term Frequency Wordcloud')
-    
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return (NULL)
+    }else{
+      tsum = wordcounts()
+      tsum = tsum[order(tsum, decreasing = T)]
+      dtm.word.cloud(count = tsum,max.words = input$max,title = 'Term Frequency Wordcloud')
+    }
   })
   
-  output$data <- renderPrint({
-    selected=input$POS
-    selected
+  output$cooccureance <- renderPlot({
+    if(is.null(input$file) || is.null(input$udpipefile) )
+    {
+      return (NULL)
+    }else{
+      wordnetwork <- head(cooccurrence.obj(), 15)
+      wordnetwork <- graph_from_data_frame(wordnetwork)
+      ggraph(wordnetwork, layout = "fr") +
+        geom_edge_link(aes(width = cooc, edge_alpha = cooc)) +
+        geom_node_text(aes(label = name), col = "red", size = 10) +
+        theme_graph(base_family = "Arial Narrow" , title_face = "bold" , text_colour = "black" ,base_size = 11,title_margin = 10) +
+        labs(title = "Co-occurance of a word")
+    }
   })
   
+  
+  output$downloadData1 <- downloadHandler(
+    filename = function() { "Nokia_Lumia_reviews.txt" },
+    content = function(file1) {
+      writeLines(readLines("data/Nokia_Lumia_reviews.txt"), file1)
+    }
+  )
+  
+  
+  output$downloadData2 <- downloadHandler(
+    filename = function() { "english-ud-2.0-170801.udpipe" },
+    content = function(file2) {
+      writeLines(readLines("data/english-ud-2.0-170801.udpipe"), file2)
+    }
+  )
 })
--- ui.R
+++ ui.R
@@ -13,6 +13,9 @@ library(magrittr)
 library(tidytext)
 library(dplyr)
 library(tidyr)
+library(igraph)
+library(ggraph)
+library(ggplot2)
 
 
 shinyUI(fluidPage(
@@ -26,12 +29,14 @@ shinyUI(fluidPage(
     
     fileInput("udpipefile", "Upload Udpipe file"),
     
-    checkboxGroupInput("POS", "POS:",
-                       c("Adjective " = "JJ",
-                         "Noun" = "NN",
-                         "proper noun" ="NNP",
-                         "adverb" = "RB",
-                          "verb"="VB")
+    checkboxGroupInput("POSselected", "POS:",
+                       c("Adjective " = "ADJ",
+                         "Noun" = "NOUN",
+                         "proper noun" ="PRON",
+                         "adverb" = "ADV",
+                          "verb"="VERB")
+                       ,
+                       selected = c("ADJ","NOUN","PRON")
                        ),
     submitButton(text = "Apply Changes", icon("refresh"))
     
@@ -40,11 +45,11 @@ shinyUI(fluidPage(
   # Main Panel:
   mainPanel( 
     tabsetPanel(type = "tabs",
-                #
+                
                 tabPanel("Overview",h4(p("How to use this App")),
                          
                          p("To use this app you need a document corpus in txt file format. Make sure each document is separated from another document with a new line character.
-                           To do basic Text Analysis in your text corpus, click on Browse in left-sidebar panel and upload the txt file. Once the file is uploaded it will do the computations in 
+                           To do basic NLP function in your text corpus, click on Browse in left-sidebar panel and upload the txt file of a particular language and Udpipe file of that particular language. Once the file is uploaded it will do the computations in 
                            back-end with default inputs and accordingly results will be displayed in various tabs.", align = "justify"),
                          p("If you wish to change the input, modify the input in left side-bar panel and click on Apply changes. Accordingly results in other tab will be refreshed
                            ", align = "Justify"),
@@ -52,17 +57,39 @@ shinyUI(fluidPage(
                          p("You might observe no change in the outputs after clicking 'Apply Changes'. Wait for few seconds. As soon as all the computations
                            are over in back-end results will be refreshed",
                            align = "justify"),
-                         #, height = 280, width = 400
+                        
+                         downloadButton('downloadData1', 'Download Nokia Lumia reviews txt file'),br(),br(),
+                         p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
+                         img(src = "example1.png"),br(),
+                         
+                         downloadButton('downloadData2', 'Download english udpipe file'),br(),br(),
+                         p("Please note that download will not work with RStudio interface. Download will work only in web-browsers. So open this app in a web-browser and then download the example file. For opening this app in web-browser click on \"Open in Browser\" as shown below -"),
+                         img(src = "example1.png"),br(),
+                         
                          verbatimTextOutput("start")),
+                    
+                       tabPanel("Table of annotated documents", 
+                         dataTableOutput('datatableOutput')),      
                 
-                tabPanel("First result",h4(p("Output")),
-                         
-                         
-                         #, height = 280, width = 400
-                          verbatimTextOutput("data"),
-                          verbatimTextOutput("wordcloud")
-                         )
+                
+                        tabPanel("Frequency of occurrence",
+                                 h4(p("UPOS (Universal Parts of Speech)\n frequency of occurrence")),
+                          plotOutput('posFreq')
+                         ),
+                        
+                        tabPanel("Most occurance",
+                                 h4(p("Most occurance of a word based on the POS selection from checkbox")),
+                         verbatimTextOutput("selectedPOS"),
+                         plotOutput('mostOcc'),
+                         h4(p("Word Cloud based on the occurance")),
+                         plotOutput('wordCloudFre')
+                        ) ,
+                        
+                       tabPanel('co-occurrences',h4(p("co-occurrences")),
+                                plotOutput('cooccureance')
+                        )
+                    
                     )
     )
-)
   )
+)
